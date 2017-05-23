dcdbc87ae59d1e9ed7c14289bb0fbb4a0122909e
leosonus <leosonus@gmail.com>
2017-05-24 01:18:07 +0900

    example
    first

---
+++ app/src/main/java/com/example/leomac/asynctaskex/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.leomac.asynctaskex;
+
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private CheckTypesTask task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        task = new CheckTypesTask();
+
+        mButton = (Button) findViewById(R.id.octopus_progress);
+        mButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task.execute();
+            }
+        });
+
+    }
+
+    private class CheckTypesTask extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog asyncDialog = new ProgressDialog(
+                MainActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            asyncDialog.setMessage("로딩중입니다..");
+            asyncDialog.setCancelable(false);
+
+            // show dialog
+            asyncDialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+                //for (int i = 0; i < 5; i++) {
+                //asyncDialog.setProgress(i * 30);
+                Thread.sleep(5000);
+                //}
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            asyncDialog.dismiss();
+            super.onPostExecute(result);
+        }
+    }
+}
+
+
+
